a629899fee30b8999d27173c4204eeacd85c3ef0
akapan <akapan.r@hitachi-tstv.com>
2019-01-15 09:58:51 +0700

    Initial Commit

---
+++ HelloSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by TST-APP-02 on 15/1/2562 BE.
+//  Copyright © 2562 Akapan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
