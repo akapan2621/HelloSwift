f0a6fd0cb8caf69f0a7b0e092cd12870d8f4ba7c
akapan <akapan.r@hitachi-tstv.com>
2019-01-15 10:42:26 +0700

    Change Label Successed

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    Explicit
+    var name: String = "Akapan"
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }   // Main Function
+    
+    
+    @IBAction func clickMeButton(_ sender: UIButton) {
+        titleLabel.text = name
     }
+    
 
 
-}
+}   // Main Class
 
